30f322a71e0c3a249b9ab8efbdc3a49dcc58a01f
FankYu <124827467+FankYu@users.noreply.github.com>
2025-12-06 21:27:03 -0800

    Add files via upload
    Outputs now to the I2S tx and fixed any inconsistency now that we drive rx clk/ws

--- hardware_top.v
+++ hardware_top.v
@@ -2,29 +2,22 @@ module hardware_top (
 	input wire clk,
 	input wire rst_n,
 
+	input wire ws,
+	input wire sck,
+
 	// Error mic
-	input wire ws_e,
-	input wire sck_e,
-	input wire sd_e,
-
+	output wire sd_e,
 	// Reference mic
-	input wire ws_x,
-	input wire sck_x,
-	input wire sd_x,
-
+	output wire sd_x,
 	// Audio input
-	input wire ws_a,
-	input wire sck_a,
-	input wire sd_a,
-
+	output wire sd_a,
 	// LMS step size input
-	input wire ws_u,
-	input wire sck_u,
-	input wire sd_u,
+	output wire sd_u,
 
-	// Outputs (simulation purposes for now)
-	output wire signed [15:0] out_sample,	// FIR filter output
-	output wire               out_valid,    // output valid signal
+	//OUT TO DAC
+	output wire ws_out,
+	output wire sck_out,
+	output wire sd_out,
 
 	// Initialization bits (serially shifted in on reset)
 	input wire init_in,
@@ -43,10 +36,10 @@ wire x_vld, x_rdy;
 wire a_vld, a_rdy;
 wire u_vld, u_rdy;
 
-i2s_rx i2s_rx_e (.clk(clk), .rst_n(rst_n), .ws(ws_e), .sck(sck_e), .sd(sd_e), .dout(e_in), .dout_vld(e_vld), .dout_rdy(e_rdy));
-i2s_rx i2s_rx_x (.clk(clk), .rst_n(rst_n), .ws(ws_x), .sck(sck_x), .sd(sd_x), .dout(x_in), .dout_vld(x_vld), .dout_rdy(x_rdy));
-i2s_rx i2s_rx_a (.clk(clk), .rst_n(rst_n), .ws(ws_a), .sck(sck_a), .sd(sd_a), .dout(a_in), .dout_vld(a_vld), .dout_rdy(a_rdy));
-i2s_rx i2s_rx_u (.clk(clk), .rst_n(rst_n), .ws(ws_u), .sck(sck_u), .sd(sd_u), .dout(u_in), .dout_vld(u_vld), .dout_rdy(u_rdy));
+i2s_rx i2s_rx_e (.clk(clk), .rst_n(rst_n), .ws(ws), .sck(sck), .sd(sd_e), .dout(e_in), .dout_vld(e_vld), .dout_rdy(e_rdy), .sck_period(i2s_in_clk_period));
+i2s_rx i2s_rx_x (.clk(clk), .rst_n(rst_n), .ws(ws), .sck(sck), .sd(sd_x), .dout(x_in), .dout_vld(x_vld), .dout_rdy(x_rdy), .sck_period(i2s_in_clk_period));
+i2s_rx i2s_rx_a (.clk(clk), .rst_n(rst_n), .ws(ws), .sck(sck), .sd(sd_a), .dout(a_in), .dout_vld(a_vld), .dout_rdy(a_rdy), .sck_period(i2s_in_clk_period));
+i2s_rx i2s_rx_u (.clk(clk), .rst_n(rst_n), .ws(ws), .sck(sck), .sd(sd_u), .dout(u_in), .dout_vld(u_vld), .dout_rdy(u_rdy), .sck_period(i2s_in_clk_period));
 
 // Handshake with controller
 wire data_valid;
@@ -106,6 +99,20 @@ anc_top anc_top_inst (
 	.out_valid(out_valid),
         .weight_inject  ( weight_inject )
 );
+
+i2s_tx i2s_tx_inst (
+    .clk        (clk),
+    .rst_n      (rst_n),
+    .bclk_period(i2s_out_clk_period),
+
+    .sample_vld (out_valid),
+    .sample     (out_sample),
+
+    .lr_clk     (ws_out),
+    .bclk       (sck_out),
+    .dout       (sd_out)
+);
+
 
 // Plus stuff for DAC and interface with FPGA
 // if out_valid (one pulse) then send out output sample
